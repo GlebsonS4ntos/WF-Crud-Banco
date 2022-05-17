8d67e724c5a6592383dae90ff74fc29f08adef0a
Glebson Santos <glebsonsantos@outlook.pt>
2022-05-16 22:15:48 -0300

    Crud com acesso ao banco no WF

---
+++ Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace App11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnDeletar = new System.Windows.Forms.Button();
+            this.btnAtualizar = new System.Windows.Forms.Button();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtQuantidade = new System.Windows.Forms.TextBox();
+            this.txtPreco = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataSet1 = new System.Data.DataSet();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(16, 26);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(589, 359);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // btnDeletar
+            // 
+            this.btnDeletar.Location = new System.Drawing.Point(39, 436);
+            this.btnDeletar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnDeletar.Name = "btnDeletar";
+            this.btnDeletar.Size = new System.Drawing.Size(160, 70);
+            this.btnDeletar.TabIndex = 1;
+            this.btnDeletar.Text = "Delete";
+            this.btnDeletar.UseVisualStyleBackColor = true;
+            this.btnDeletar.Click += new System.EventHandler(this.btnDeletar_Click);
+            // 
+            // btnAtualizar
+            // 
+            this.btnAtualizar.Location = new System.Drawing.Point(235, 436);
+            this.btnAtualizar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnAtualizar.Name = "btnAtualizar";
+            this.btnAtualizar.Size = new System.Drawing.Size(173, 70);
+            this.btnAtualizar.TabIndex = 1;
+            this.btnAtualizar.Text = "Update";
+            this.btnAtualizar.UseVisualStyleBackColor = true;
+            this.btnAtualizar.Click += new System.EventHandler(this.btnAtualizar_Click);
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(452, 436);
+            this.btnInsert.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(173, 70);
+            this.btnInsert.TabIndex = 1;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(783, 229);
+            this.txtNome.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(252, 22);
+            this.txtNome.TabIndex = 2;
+            this.txtNome.TextChanged += new System.EventHandler(this.txtNome_TextChanged);
+            // 
+            // txtQuantidade
+            // 
+            this.txtQuantidade.Location = new System.Drawing.Point(782, 363);
+            this.txtQuantidade.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtQuantidade.Name = "txtQuantidade";
+            this.txtQuantidade.Size = new System.Drawing.Size(252, 22);
+            this.txtQuantidade.TabIndex = 2;
+            this.txtQuantidade.TextChanged += new System.EventHandler(this.txtQuantidade_TextChanged);
+            // 
+            // txtPreco
+            // 
+            this.txtPreco.Location = new System.Drawing.Point(783, 293);
+            this.txtPreco.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtPreco.Name = "txtPreco";
+            this.txtPreco.Size = new System.Drawing.Size(252, 22);
+            this.txtPreco.TabIndex = 2;
+            this.txtPreco.TextChanged += new System.EventHandler(this.txtPreco_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(724, 233);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nome:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(687, 367);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Quantidade:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(724, 297);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Preço:";
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "NewDataSet";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Showcard Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(773, 95);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(214, 50);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Produto";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(783, 181);
+            this.txtId.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(57, 22);
+            this.txtId.TabIndex = 2;
+            this.txtId.TextChanged += new System.EventHandler(this.txtNome_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(749, 185);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(21, 16);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Id:\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1105, 554);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPreco);
+            this.Controls.Add(this.txtQuantidade);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.btnAtualizar);
+            this.Controls.Add(this.btnDeletar);
+            this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnDeletar;
+        private System.Windows.Forms.Button btnAtualizar;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtQuantidade;
+        private System.Windows.Forms.TextBox txtPreco;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Data.DataSet dataSet1;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace App11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            AtualizarDataGrid();
+            btnAtualizar.Enabled = false;
+            btnDeletar.Enabled = false;
+            btnInsert.Enabled = false;
+            txtId.Enabled = false;
+        }
+
+        private void AtualizarDataGrid()
+        {
+            string con = @"Data Source=DESKTOP-7Q70RLR\SQLEXPRESS;Initial Catalog=bdVendas;Integrated Security=True";
+            
+            SqlConnection sqlCon = new SqlConnection(con);
+            sqlCon.Open();
+            SqlCommand cmd = new SqlCommand("Select * From Produto", sqlCon);
+            cmd.ExecuteNonQuery();
+
+            SqlDataAdapter ad = new SqlDataAdapter();
+
+            DataSet ds = new DataSet();
+            ad.SelectCommand = cmd;
+            ad.Fill(ds);
+
+            dataGridView1.DataSource = ds;
+            dataGridView1.DataMember = ds.Tables[0].TableName;
+
+            sqlCon.Close();
+        }
+
+        private void LimparCampos()
+        {
+            txtId.Text = "";
+            txtNome.Text = "";
+            txtPreco.Text = "";
+            txtQuantidade.Text = "";
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            string con = @"Data Source=DESKTOP-7Q70RLR\SQLEXPRESS;Initial Catalog=bdVendas;Integrated Security=True";
+            string insert = "Insert into Produto(NomeProduto, Preco, Quantidade) values" +
+                "('" + txtNome.Text + "','" + decimal.Parse(txtPreco.Text) +"','"+int.Parse(txtQuantidade.Text)+"')";
+            SqlConnection sqlCon = new SqlConnection(con);
+
+            try
+            {
+                sqlCon.Open();
+                SqlCommand cmd = new SqlCommand(insert, sqlCon);
+                int i = cmd.ExecuteNonQuery();
+                if (i > 0)
+                    MessageBox.Show("Produto Cadastrado com sucesso");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally {
+                sqlCon.Close();
+                LimparCampos();
+                AtualizarDataGrid();
+            }
+        }
+
+        private void txtPreco_TextChanged(object sender, EventArgs e)
+        {
+            if(txtPreco.Text != "" && txtNome.Text != "" && txtQuantidade.Text != "")
+                btnInsert.Enabled = true;
+        }
+
+        private void txtNome_TextChanged(object sender, EventArgs e)
+        {
+            if (txtPreco.Text != "" && txtNome.Text != "" && txtQuantidade.Text != "")
+                btnInsert.Enabled = true;
+        }
+
+        private void txtQuantidade_TextChanged(object sender, EventArgs e)
+        {
+            if (txtPreco.Text != "" && txtNome.Text != "" && txtQuantidade.Text != "")
+                btnInsert.Enabled = true;
+        }
+
+        private void btnDeletar_Click(object sender, EventArgs e)
+        {
+            string con = @"Data Source=DESKTOP-7Q70RLR\SQLEXPRESS;Initial Catalog=bdVendas;Integrated Security=True";
+            SqlConnection sqlCom = new SqlConnection(con);
+            string delete = "Delete from Produto where ProdutoId = '" + txtId.Text +"'"; 
+
+            SqlCommand cmd = new SqlCommand(delete, sqlCom);
+            cmd.CommandType = CommandType.Text;
+
+            try
+            {
+                sqlCom.Open();
+                int i = cmd.ExecuteNonQuery();
+                if (i > 0)
+                {
+                    MessageBox.Show("Deletado com Sucesso");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlCom.Close();
+                LimparCampos();
+                AtualizarDataGrid();
+                btnDeletar.Enabled = false;
+                btnAtualizar.Enabled = false;
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtId.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            txtNome.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            txtPreco.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            txtQuantidade.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+
+            btnAtualizar.Enabled = true;
+            btnDeletar.Enabled = true;
+            btnInsert.Enabled = false;
+        }
+
+        private void btnAtualizar_Click(object sender, EventArgs e)
+        {
+            string con = @"Data Source=DESKTOP-7Q70RLR\SQLEXPRESS;Initial Catalog=bdVendas;Integrated Security=True";
+            SqlConnection sqlCom = new SqlConnection(con);
+
+            string sql = "Update produto set NomeProduto = '" + txtNome.Text + "', Preco = '" + double.Parse(txtPreco.Text) +"', Quantidade= '" + txtQuantidade.Text + "' where ProdutoId = '" + txtId.Text + "'";
+
+            SqlCommand cmd = new SqlCommand(sql, sqlCom);
+            cmd.CommandType = CommandType.Text;
+
+            try
+            {
+                sqlCom.Open();
+                int i = cmd.ExecuteNonQuery();
+                if (i > 0)
+                {
+                    MessageBox.Show("Alterado com Sucesso");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlCom.Close();
+                LimparCampos();
+                AtualizarDataGrid();
+                btnDeletar.Enabled = false;
+                btnAtualizar.Enabled = false;
+            }
+        }
+    }
+}
